d383c940211eefa17da000fe3e8390268a0fc7d5
giovannidisiena <giodisiena@gmail.com>
2021-02-12 10:24:45 +0000

    Add try/catch to allocateSeigniorage call

--- index.js
+++ index.js
@@ -1,50 +1,57 @@
-require('dotenv').config();
-const ethers = require('ethers');
-let CronJob = require('cron').CronJob;
+require('dotenv').config()
+const ethers = require('ethers')
+let CronJob = require('cron').CronJob
 
 const init = async () => {
   const provider = ethers.getDefaultProvider('mainnet', {
-    infura: process.env.INFURA_ID
-  });
-  const signer = new ethers.Wallet(process.env.PRIVATE_KEY);
-  const account = signer.connect(provider);
+    infura: process.env.INFURA_ID,
+  })
+  const signer = new ethers.Wallet(process.env.PRIVATE_KEY)
+  const account = signer.connect(provider)
   const linkswap = new ethers.Contract(
     '0x109d123cB3960BFa51a284a9B14EE7dB5CfBca59',
-    ['function allocateSeigniorage() external',
-     'function getSeigniorageOraclePrice() public view returns (uint)'],
+    [
+      'function allocateSeigniorage() external',
+      'function getSeigniorageOraclePrice() public view returns (uint)',
+    ],
     account
-  );
-  let gasPrice;
-  let oraclePrice;
+  )
+  let gasPrice
+  let oraclePrice
   try {
-    gasPrice = await provider.getGasPrice();
+    gasPrice = await provider.getGasPrice()
   } catch (err) {
-    console.log(err);
-    gasPrice = ethers.BigNumber.from("100000000000");
+    console.log(err)
+    gasPrice = ethers.BigNumber.from('100000000000')
   }
-  gasPrice.add(ethers.BigNumber.from("10000000000"));
+  gasPrice.add(ethers.BigNumber.from('10000000000'))
   try {
-    oraclePrice = await linkswap.getSeigniorageOraclePrice();
+    oraclePrice = await linkswap.getSeigniorageOraclePrice()
   } catch (err) {
-     console.log(err);
-     console.log('Failed to getSeigniorageOraclePrice... terminating.')
-     return;
+    console.log(err)
+    console.log('Failed to getSeigniorageOraclePrice... terminating.')
+    return
+  }
+  if (oraclePrice > ethers.BigNumber.from('1000000000000000000')) {
+    let gasLimit = ethers.BigNumber.from(process.env.GAS_LIMIT)
+    try {
+      const tx = await linkswap.allocateSeigniorage({ gasPrice, gasLimit })
+      console.log('Transaction hash: ${tx.hash}')
+
+      const receipt = await tx.wait()
+
+      console.log('Transaction was mined in block ${receipt.blockNumber}')
+    } catch (err) {
+      console.log(err)
     }
-  if (oraclePrice > ethers.BigNumber.from("1000000000000000000")) {
-    let gasLimit = ethers.BigNumber.from(process.env.GAS_LIMIT);
-    const tx = await linkswap.allocateSeigniorage(
-      { gasPrice, gasLimit }
-    );
-    console.log('Transaction hash: ${tx.hash}');
-  
-    const receipt = await tx.wait();
-    console.log('Transaction was mined in block ${receipt.blockNumber}');
   } else {
-    console.log('SeigniorageOraclePrice > 1e18, so allocateSeigniorage not called');
+    console.log(
+      'SeigniorageOraclePrice > 1e18, so allocateSeigniorage not called'
+    )
   }
 }
 
-const job = new CronJob('0 0 * * *', function() {
-  init();
-});
-job.start();
+const job = new CronJob('0 0 * * *', function () {
+  init()
+})
+job.start()
